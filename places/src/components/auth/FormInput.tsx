031916953641f4b4dc00371ad149493b18261620
Michail Chatzis <mchatzis3@gmail.com>
2024-11-10 12:21:51 +0100

    Added some css transistions

--- places/src/components/auth/FormInput.tsx
+++ places/src/components/auth/FormInput.tsx
@@ -1,18 +1,18 @@
 interface InputFieldProps {
-    id: string;
-    name: string;
-    type?: string;
-    placeholder?: string;
-  }
+  id: string;
+  name: string;
+  type?: string;
+  placeholder?: string;
+}
 
 export default function FormInput({ id, name, type = "text", placeholder }: InputFieldProps) {
-    return (
-      <input
-        id={id}
-        name={name}
-        type={type}
-        placeholder={placeholder}
-        className="border border-gray-300 rounded-md px-4 py-2 w-full"
-      />
-    );
-  }
+  return (
+    <input
+      id={id}
+      name={name}
+      type={type}
+      placeholder={placeholder}
+      className="border border-gray-300 rounded-md px-4 py-2 w-full text-[var(--primary-color)]"
+    />
+  );
+}
--- places/src/components/auth/LoginForm.tsx
+++ places/src/components/auth/LoginForm.tsx
@@ -1,16 +1,16 @@
-import { authenticate } from '@/app/actions/auth/authenticate'
-import Link from "next/link";
+import { authenticate } from '@/app/actions/auth/authenticate';
 import FormInput from '@/components/auth/FormInput';
+import Link from "next/link";
 
-export default function LoginForm(){
-    return (
-        <form action={authenticate} className='min-w-32 max-w-52 m-2'>
-          <FormInput id="email" name="email" type="email" placeholder="email" />
-          <FormInput id="password" name="password" type="password" placeholder="password" />
-          <div className="w-full pt-1">
-            <button type="submit" className='w-full border rounded-full p-2 bg-gray-400 hover:bg-gray-300'>Login</button>
-            <Link href='/auth/register' className='text-blue-700 rounded-full text-center flex items-center justify-center p-3 text-sm'>sign up</Link>
-          </div>
-        </form>
-      );
+export default function LoginForm() {
+  return (
+    <form action={authenticate} className='min-w-32 max-w-52 m-2'>
+      <FormInput id="email" name="email" type="email" placeholder="email" />
+      <FormInput id="password" name="password" type="password" placeholder="password" />
+      <div className="w-full pt-1">
+        <button type="submit" className='w-full border rounded-full p-2 bg-gray-400 hover:bg-gray-300 transition-colors duration-300'>Login</button>
+        <Link href='/auth/register' className='text-blue-700 rounded-full text-center flex items-center justify-center p-3 text-sm transform hover:scale-110 transition-transform duration-150'>sign up</Link>
+      </div>
+    </form>
+  );
 }
