ab0868ca1a2955e2d6038ecc86de2796f29e9233
Michail Chatzis <mchatzis3@gmail.com>
2025-01-07 10:17:43 +0100

    Fixed S3 dir for images

--- places/src/components/display/PlaceInfo.tsx
+++ places/src/components/display/PlaceInfo.tsx
@@ -3,7 +3,7 @@ import { Place } from "@/lib/redux/slices/mapStateSlice";
 export default function PlaceInfo({ selectedPlace, className = '' }: { selectedPlace: Place, className?: string }) {
     return (
         <div className={`${className}`}>
-            <p>{selectedPlace.properties.title}</p>
+            <p>{selectedPlace.properties.name}</p>
             <p>{selectedPlace.properties.description}</p>
         </div>
     )
