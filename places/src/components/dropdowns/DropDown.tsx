ffc5bf05d92c8a257d8ba71453acd4220c178f27
Michail Chatzis <mchatzis3@gmail.com>
2025-01-27 11:36:17 +0100

    Corrected visual bug with wrong border radius

--- places/src/components/dropdowns/DropDown.tsx
+++ places/src/components/dropdowns/DropDown.tsx
@@ -6,13 +6,13 @@ interface DropdownProps {
 }
 export function DropDown({ options, onMouseDown }: DropdownProps) {
     return (
-        <div className="w-full h-fit max-h-[15vh] relative z-[var(--z-popup)] rounded-xl overflow-y-scroll bg-discrete">
+        <div className="w-full h-fit max-h-[15vh] relative z-[var(--z-popup)] rounded-xl overflow-y-auto bg-discrete">
             <ul className='w-full h-full text-base text-white/90'>
                 {options.map((option, index) => {
                     return (
                         <li
                             key={index}
-                            className='block cursor-pointer px-1 py-1 border border-transparent hover:text-primary hover:rounded-lg
+                            className='block cursor-pointer px-1 py-1 border border-transparent hover:text-primary hover:rounded-xl
                                 hover:border hover:border-primary'
                             onMouseDown={() => onMouseDown(option)}
                         >
